5ffc67a372c9a9bc4bd8aea469edb0f2042b1503
jannicksostack <142694256+jannicksostack@users.noreply.github.com>
2024-03-18 08:26:16 +0100

    iukj

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Jannick Sostack!");
+            Console.WriteLine("Hello, Fra VS Sostack!");
         }
     }
 }
